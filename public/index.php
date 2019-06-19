b83da08e5c1c50e803c139d3ce55b1054b41444a
drohnwynandrt <dw.ramdattewari@gmail.com>
2019-06-19 12:55:39 +0200

    Cloned files from Floris for the chatbot

--- public/index.php
+++ public/index.php
@@ -3,7 +3,164 @@
 require '../vendor/autoload.php';
 
 
+use LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder;
+use LINE\LINEBot\TemplateActionBuilder\Uri\AltUriBuilder;
+use LINE\LINEBot\Constant\Flex\ComponentButtonHeight;
+use LINE\LINEBot\Constant\Flex\ComponentButtonStyle;
+use LINE\LINEBot\Constant\Flex\ComponentFontSize;
+use LINE\LINEBot\Constant\Flex\ComponentFontWeight;
+use LINE\LINEBot\Constant\Flex\ComponentIconSize;
+use LINE\LINEBot\Constant\Flex\ComponentImageAspectMode;
+use LINE\LINEBot\Constant\Flex\ComponentImageAspectRatio;
+use LINE\LINEBot\Constant\Flex\ComponentImageSize;
+use LINE\LINEBot\Constant\Flex\ComponentLayout;
+use LINE\LINEBot\Constant\Flex\ComponentMargin;
+use LINE\LINEBot\Constant\Flex\ComponentSpaceSize;
+use LINE\LINEBot\Constant\Flex\ComponentSpacing;
+use LINE\LINEBot\MessageBuilder\FlexMessageBuilder;
+use LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\BoxComponentBuilder;
+use LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\ButtonComponentBuilder;
+use LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\IconComponentBuilder;
+use LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\ImageComponentBuilder;
+use LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\SpacerComponentBuilder;
+use LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\TextComponentBuilder;
+use LINE\LINEBot\MessageBuilder\Flex\ContainerBuilder\BubbleContainerBuilder;
+
+
 use \LINE\LINEBot\SignatureValidator as SignatureValidator;
+
+
+/**
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ */
+class FlexSampleRestaurant
+{
+    /**
+     * Create sample restaurant flex message
+     *
+     * @return \LINE\LINEBot\MessageBuilder\FlexMessageBuilder
+     */
+    public static function get()
+    {
+        return FlexMessageBuilder::builder()
+            ->setAltText('Restaurant')
+            ->setContents(
+                BubbleContainerBuilder::builder()
+                    ->setHero(self::createHeroBlock())
+                    ->setBody(self::createBodyBlock())
+                    ->setFooter(self::createFooterBlock())
+            );
+    }
+    private static function createHeroBlock()
+    {
+        return ImageComponentBuilder::builder()
+            ->setUrl('https://www.amrathhotelempereur.nl/heading/restaurant_4.jpg')
+            ->setSize(ComponentImageSize::FULL)
+            ->setAspectRatio(ComponentImageAspectRatio::R20TO13)
+            ->setAspectMode(ComponentImageAspectMode::COVER)
+            ->setAction(
+                new UriTemplateActionBuilder(
+                    null,
+                    'https://example.com',
+                    new AltUriBuilder('https://example.com#desktop')
+                )
+            );
+    }
+    private static function createBodyBlock()
+    {
+        $title = TextComponentBuilder::builder()
+            ->setText('Brown Cafe')
+            ->setWeight(ComponentFontWeight::BOLD)
+            ->setSize(ComponentFontSize::XL);
+        $goldStar = IconComponentBuilder::builder()
+            ->setUrl('https://example.com/gold_star.png')
+            ->setSize(ComponentIconSize::SM);
+        $grayStar = IconComponentBuilder::builder()
+            ->setUrl('https://example.com/gray_star.png')
+            ->setSize(ComponentIconSize::SM);
+        $point = TextComponentBuilder::builder()
+            ->setText('4.0')
+            ->setSize(ComponentFontSize::SM)
+            ->setColor('#999999')
+            ->setMargin(ComponentMargin::MD)
+            ->setFlex(0);
+        $review = BoxComponentBuilder::builder()
+            ->setLayout(ComponentLayout::BASELINE)
+            ->setMargin(ComponentMargin::MD)
+            ->setContents([$goldStar, $goldStar, $goldStar, $goldStar, $grayStar, $point]);
+        $place = BoxComponentBuilder::builder()
+            ->setLayout(ComponentLayout::BASELINE)
+            ->setSpacing(ComponentSpacing::SM)
+            ->setContents([
+                TextComponentBuilder::builder()
+                    ->setText('Place')
+                    ->setColor('#aaaaaa')
+                    ->setSize(ComponentFontSize::SM)
+                    ->setFlex(1),
+                TextComponentBuilder::builder()
+                    ->setText('Miraina Tower, 4-1-6 Shinjuku, Tokyo')
+                    ->setWrap(true)
+                    ->setColor('#666666')
+                    ->setSize(ComponentFontSize::SM)
+                    ->setFlex(5)
+            ]);
+        $time = BoxComponentBuilder::builder()
+            ->setLayout(ComponentLayout::BASELINE)
+            ->setSpacing(ComponentSpacing::SM)
+            ->setContents([
+                TextComponentBuilder::builder()
+                    ->setText('Time')
+                    ->setColor('#aaaaaa')
+                    ->setSize(ComponentFontSize::SM)
+                    ->setFlex(1),
+                TextComponentBuilder::builder()
+                    ->setText('10:00 - 23:00')
+                    ->setWrap(true)
+                    ->setColor('#666666')
+                    ->setSize(ComponentFontSize::SM)
+                    ->setFlex(5)
+            ]);
+        $info = BoxComponentBuilder::builder()
+            ->setLayout(ComponentLayout::VERTICAL)
+            ->setMargin(ComponentMargin::LG)
+            ->setSpacing(ComponentSpacing::SM)
+            ->setContents([$place, $time]);
+        return BoxComponentBuilder::builder()
+            ->setLayout(ComponentLayout::VERTICAL)
+            ->setContents([$title, $review, $info]);
+    }
+    private static function createFooterBlock()
+    {
+        $callButton = ButtonComponentBuilder::builder()
+            ->setStyle(ComponentButtonStyle::LINK)
+            ->setHeight(ComponentButtonHeight::SM)
+            ->setAction(
+                new UriTemplateActionBuilder(
+                    'CALL',
+                    'https://example.com',
+                    new AltUriBuilder('https://example.com#desktop')
+                )
+            );
+        $websiteButton = ButtonComponentBuilder::builder()
+            ->setStyle(ComponentButtonStyle::LINK)
+            ->setHeight(ComponentButtonHeight::SM)
+            ->setAction(
+                new UriTemplateActionBuilder(
+                    'WEBSITE',
+                    'https://example.com',
+                    new AltUriBuilder('https://example.com#desktop')
+                )
+            );
+        $spacer = new SpacerComponentBuilder(ComponentSpaceSize::SM);
+        return BoxComponentBuilder::builder()
+            ->setLayout(ComponentLayout::VERTICAL)
+            ->setSpacing(ComponentSpacing::SM)
+            ->setFlex(0)
+            ->setContents([$callButton, $websiteButton, $spacer]);
+    }
+}
+
+
 
 
 // initiate app
@@ -43,9 +200,39 @@ $signature = $_SERVER['HTTP_X_LINE_SIGNATURE'];
 	foreach ($data['events'] as $event)
 	{
 		$userMessage = $event['message']['text'];
-		if(strtolower($userMessage) == 'hallo')
+
+		$thesender = $event['source']['userId'];
+
+      file_put_contents('php://stderr', " ----- ");
+
+
+	   if(strtolower($userMessage) == 'ok2')
 		{
-			$message = "Hallo Gorilla";
+			/*
+			$res = file_get_contents("demo.json");
+			$res = utf8_encode($res);
+        	$res = json_decode($res);
+*/
+
+		// $data = new \LINE\LINEBot\MessageBuilder\FlexMessageBuilder("Allernatief", $res);
+		  
+		  	$data = FlexSampleRestaurant::get();
+
+          file_put_contents('php://stderr', 'reply data: ' . print_r($data, true));
+
+
+			$result = $bot->replyMessage($event['replyToken'], $data);
+
+			//return $response->withJson($result->getJSONDecodedBody(), $result->getHTTPStatus());
+
+			return $result->getHTTPStatus() . ' ' . $result->getRawBody();
+		
+		}
+
+
+	   if(strtolower($userMessage) == 'wie')
+		{
+			$message = $thesender;
             $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message);
 			$result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
 			return $result->getHTTPStatus() . ' ' . $result->getRawBody();
@@ -53,14 +240,6 @@ $signature = $_SERVER['HTTP_X_LINE_SIGNATURE'];
 		}
 
 
-	   if(strtolower($userMessage) == 'afspraak')
-		{
-			$message = "Kun je vanmiddag om 15:00 ?";
-            $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message);
-			$result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
-			return $result->getHTTPStatus() . ' ' . $result->getRawBody();
-		
-		}
 
 
      if(strtolower($userMessage) == 'sticker')
@@ -68,6 +247,20 @@ $signature = $_SERVER['HTTP_X_LINE_SIGNATURE'];
 	
             $mysticker = new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder("11538", "51626501");
 			$result = $bot->replyMessage($event['replyToken'], $mysticker);
+			return $result->getHTTPStatus() . ' ' . $result->getRawBody();
+		
+		}
+
+	   if(strtolower($userMessage) == 'emo')
+		{
+
+	    
+		$message = "Hallo "  . json_decode('"\ud83d\ude00"');
+
+			//$message = "Hallo " . $unicodeChar;
+
+            $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message);
+			$result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
 			return $result->getHTTPStatus() . ' ' . $result->getRawBody();
 		
 		}
